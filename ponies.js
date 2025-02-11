d535211498ffea8c7665d4ba180eb6c31d90e8dd
annabudpi <ribeaua20@gmail.com>
2025-02-11 12:50:27 -0500

    Create ponies.js

---
+++ ponies.js
@@ -0,0 +1,17 @@
+fetch('ponies.json')
+    .then(response => response.json())
+    .then(ponies => {
+        const container = document.querySelector('.pony-container');
+        ponies.forEach(pony => {
+            const ponyCard = document.createElement('div');
+            ponyCard.classList.add('pony-card');
+            ponyCard.innerHTML = `
+                <img src="${pony.image}" alt="${pony.name}">
+                <h3>${pony.name}</h3>
+                <p>Breed: ${pony.breed}</p>
+                <p>Color: ${pony.color}</p>
+            `;
+            container.appendChild(ponyCard);
+        });
+    })
+    .catch(error => console.error('Error loading ponies:', error));
